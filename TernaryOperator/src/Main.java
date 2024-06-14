6492aca9f7a53350b19b09371762414e93823326
wali-zaidi-dev <valirazabbk@gmail.com>
2024-06-14 13:43:52 +0530

    Ternary Operator Second Commit

--- TernaryOperator/src/Main.java
+++ TernaryOperator/src/Main.java
@@ -1,11 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        int num = 4;
-        if(num%2==0) {
-            System.out.println("even");
-        }
-        else{
-            System.out.println("odd");
-        }
+
+//        if(num%2==0) {
+//            System.out.println("even");
+//            result = 10;
+//        }
+//        else{
+//            System.out.println("odd");
+//            result = 20;
+//            System.out.println(result);
+//        }
+        int num = 5;
+        int result;
+      //  var = condition?true:false;
+        result = num%2==0?10:20;
+        System.out.println(result);
+
     }
 }
