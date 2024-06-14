796567d188a6d4755ebc17bf57b7b7604b3e2e4f
wali-zaidi-dev <valirazabbk@gmail.com>
2024-06-14 13:27:32 +0530

    Ternary Operator Code Commit

---
+++ TernaryOperator/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int num = 4;
+        if(num%2==0) {
+            System.out.println("even");
+        }
+        else{
+            System.out.println("odd");
+        }
+    }
+}
