8cc4d338f4c05e7095b22953ca7c80bc7f4e2451
wali-zaidi-dev <valirazabbk@gmail.com>
2024-07-31 03:38:36 +0530

    doWhile code added

---
+++ Looping/Do While/Main.java
@@ -0,0 +1,9 @@
+ class DoWhile {
+    public static void main(String[] args) {
+        int x = 5;
+        do {
+            System.out.println(x);
+            x++;
+        }while (x <= 4);
+    }
+}
