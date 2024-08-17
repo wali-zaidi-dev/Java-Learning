003f2245002d81db4031f2e936ef645154337630
wali-zaidi-dev <TATA@tcs123>
2024-08-18 02:28:59 +0530

    Multi Dimensional Array Code

--- Array/src/multi_dimensional_array/Main.java
+++ Array/src/multi_dimensional_array/Main.java
@@ -2,11 +2,20 @@ package multi_dimensional_array;
 
 public class Main {
     public static void main(String[] args) {
+        // 2D Array or Multi Dimensional Array
         int[][] myArray = new int[3][4];
         for(int row = 0; row <3; row++ ){
             for(int col = 0; col <3; col++){
                 myArray[row][col]= (int) (Math.random() * 10);
                 System.out.print(myArray[row][col]+" ");
+            }
+            System.out.println();
+        }
+            System.out.println("===Break Here===");
+        // Enhanced for() loop
+        for(int[] n : myArray){
+            for(int n1 : n){
+                System.out.print(n1+" ");
             }
             System.out.println();
         }
