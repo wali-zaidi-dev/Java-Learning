965f00763b2e664f66358f9ba6777f496455e758
wali-zaidi-dev <TATA@tcs123>
2024-09-13 05:45:22 +0530

    String

---
+++ String/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+      String stringObj = new String("Hello World");
+      System.out.println(stringObj);
+        System.out.println("Concat " + stringObj);
+      System.out.println(stringObj.charAt(0));
+    }
+}
