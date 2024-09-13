72740d8a44bd61da25047ac5363f1f7dc6f7e768
wali-zaidi-dev <TATA@tcs123>
2024-09-13 05:48:14 +0530

    String

--- String/src/Main.java
+++ String/src/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
       String stringObj = new String("Hello World");
       System.out.println(stringObj);
-        System.out.println("Concat " + stringObj);
+        System.out.println("New String Concat Manually " + stringObj);
+        System.out.println(stringObj.concat(" Concat String through concat() method"));
       System.out.println(stringObj.charAt(0));
     }
 }
