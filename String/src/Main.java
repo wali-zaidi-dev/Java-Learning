023c106519078842723acf27c3a7ecb362d87b42
wali-zaidi-dev <TATA@tcs123>
2024-09-13 05:50:08 +0530

    String

--- String/src/Main.java
+++ String/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        //General String Creation
+        String myString = "Hello World";
+        //String Object
       String stringObj = new String("Hello World");
       System.out.println(stringObj);
         System.out.println("New String Concat Manually " + stringObj);
